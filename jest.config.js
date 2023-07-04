f64adec42ebc0a07555196640ee3c9a0be64d958
devshafi <frshafi49@gmail.com>
2023-07-05 03:10:40 +0600

    updated jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: { "^.+\\.ts?$": "ts-jest" },
   testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testRegex: "/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts"],
 };
